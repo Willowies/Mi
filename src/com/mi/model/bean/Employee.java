099c508a516b244ff735f492706ed37a9ec3a15e
Willowies <z654714845@126.com>
2018-07-22 20:29:24 +0800

    commit the entry classes included comment ..

---
+++ src/com/mi/model/bean/Employee.java
@@ -0,0 +1,43 @@
+package com.mi.model.bean;
+
+public class Employee {
+	
+	private int employeeId;
+	private int emploeeType;
+	private String employeeName;
+	private String employeeAccount;
+	private String employeePassword;
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public int getEmploeeType() {
+		return emploeeType;
+	}
+	public void setEmploeeType(int emploeeType) {
+		this.emploeeType = emploeeType;
+	}
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	public String getEmployeeAccount() {
+		return employeeAccount;
+	}
+	public void setEmployeeAccount(String employeeAccount) {
+		this.employeeAccount = employeeAccount;
+	}
+	public String getEmployeePassword() {
+		return employeePassword;
+	}
+	public void setEmployeePassword(String employeePassword) {
+		this.employeePassword = employeePassword;
+	}
+
+	
+
+}
