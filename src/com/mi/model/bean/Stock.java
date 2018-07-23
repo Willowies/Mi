42c55f01ef44f85b1ceeb903d66ae3b78b82048e
Willowies <z654714845@126.com>
2018-07-23 13:21:02 +0800

    上传stock

--- src/com/mi/model/bean/Stock.java
+++ src/com/mi/model/bean/Stock.java
@@ -1,5 +1,37 @@
 package com.mi.model.bean;
 
 public class Stock {
+	
+	
+	private int stockId;
+	private int productId;
+	private int totalStock;
+	private int allocatableStock;
+	public int getStockId() {
+		return stockId;
+	}
+	public void setStockId(int stockId) {
+		this.stockId = stockId;
+	}
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public int getTotalStock() {
+		return totalStock;
+	}
+	public void setTotalStock(int totalStock) {
+		this.totalStock = totalStock;
+	}
+	public int getAllocatableStock() {
+		return allocatableStock;
+	}
+	public void setAllocatableStock(int allocatableStock) {
+		this.allocatableStock = allocatableStock;
+	}
 
+
+	
 }
