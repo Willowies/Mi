c1caf05c6824ac1ec0d36f81a63aaa4f2d7206e9
Administrator <Administrator@PC-20171111VRXS>
2018-07-23 11:29:04 +0800

    上传User类

---
+++ src/com/mi/model/bean/User.java
@@ -0,0 +1,72 @@
+package com.mi.model.bean;
+
+import java.util.Date;
+
+public class User {
+	private int userId;
+	private String userName;
+	private Date userBirth;
+	private String userSex;
+	private String userPhone;
+	private String userEmail;
+	private String userPassword;
+	private String userCard;
+	private String userHead;
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public Date getUserBirth() {
+		return userBirth;
+	}
+	public void setUserBirth(Date userBirth) {
+		this.userBirth = userBirth;
+	}
+	public String getUserSex() {
+		return userSex;
+	}
+	public void setUserSex(String userSex) {
+		this.userSex = userSex;
+	}
+	public String getUserPhone() {
+		return userPhone;
+	}
+	public void setUserPhone(String userPhone) {
+		this.userPhone = userPhone;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	public String getUserCard() {
+		return userCard;
+	}
+	public void setUserCard(String userCard) {
+		this.userCard = userCard;
+	}
+	public String getUserHead() {
+		return userHead;
+	}
+	public void setUserHead(String userHead) {
+		this.userHead = userHead;
+	}
+	
+	
+	
+}
