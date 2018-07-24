9e7da3793e4e29ab2c931d096828da115a0225c3
Administrator <Administrator@PC-20171022NXER>
2018-07-24 10:00:23 +0800

    加上了售后表的实体类

---
+++ src/com/mi/model/bean/AsTable.java
@@ -0,0 +1,100 @@
+package com.mi.model.bean;
+
+import java.util.Date;
+
+public class AsTable {
+	private String asId;
+	private String asProId;
+	private String asSn;
+	private String asWay;
+	private String asBack;
+	private String asDescription;
+	private String asAddress;
+	private String asPerson;
+	private String asPhone;
+	private int asState;
+	private String asRepairProUrl;
+	private String asInvoiceUrl;
+	private Date asTime;
+	public String getAsId() {
+		return asId;
+	}
+	public void setAsId(String asId) {
+		this.asId = asId;
+	}
+	public String getAsProId() {
+		return asProId;
+	}
+	public void setAsProId(String asProId) {
+		this.asProId = asProId;
+	}
+	public String getAsSn() {
+		return asSn;
+	}
+	public void setAsSn(String asSn) {
+		this.asSn = asSn;
+	}
+	public String getAsWay() {
+		return asWay;
+	}
+	public void setAsWay(String asWay) {
+		this.asWay = asWay;
+	}
+	public String getAsBack() {
+		return asBack;
+	}
+	public void setAsBack(String asBack) {
+		this.asBack = asBack;
+	}
+	public String getAsDescription() {
+		return asDescription;
+	}
+	public void setAsDescription(String asDescription) {
+		this.asDescription = asDescription;
+	}
+	public String getAsAddress() {
+		return asAddress;
+	}
+	public void setAsAddress(String asAddress) {
+		this.asAddress = asAddress;
+	}
+	public String getAsPerson() {
+		return asPerson;
+	}
+	public void setAsPerson(String asPerson) {
+		this.asPerson = asPerson;
+	}
+	public String getAsPhone() {
+		return asPhone;
+	}
+	public void setAsPhone(String asPhone) {
+		this.asPhone = asPhone;
+	}
+	public int getAsState() {
+		return asState;
+	}
+	public void setAsState(int asState) {
+		this.asState = asState;
+	}
+	public String getAsRepairProUrl() {
+		return asRepairProUrl;
+	}
+	public void setAsRepairProUrl(String asRepairProUrl) {
+		this.asRepairProUrl = asRepairProUrl;
+	}
+	public String getAsInvoiceUrl() {
+		return asInvoiceUrl;
+	}
+	public void setAsInvoiceUrl(String asInvoiceUrl) {
+		this.asInvoiceUrl = asInvoiceUrl;
+	}
+	public Date getAsTime() {
+		return asTime;
+	}
+	public void setAsTime(Date asTime) {
+		this.asTime = asTime;
+	}
+	
+	
+	
+}
