1be21a498b90c47c4b26427fa584f3e357dec36c
Willowies <z654714845@126.com>
2018-07-28 16:38:25 +0800

    新建po包

---
+++ src/com/mi/model/po/SearchResult.java
@@ -0,0 +1,21 @@
+package com.mi.model.po;
+
+import java.util.List;
+
+import com.mi.model.bean.Product;
+
+public class SearchResult {
+	
+	
+	private List<Product> products;
+
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
+	
+	
+}
