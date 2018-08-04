f115248f7902b977f8a9ba426f89859885004256
Willowies <z654714845@126.com>
2018-08-04 21:24:42 +0800

    删除没有用的html文件

---
+++ WebContent/js/myOwnHeadJs.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	$(".link-category").hover(function() {
+		$(".site-category").css("display", "block");
+	}, function() {
+		$(".site-category").css("display", "none");
+	});
+	$(".site-category").hover(function() {
+		$(".site-category").css("display", "block");
+	}, function() {
+		$(".site-category").css("display", "none");
+	});
+});
