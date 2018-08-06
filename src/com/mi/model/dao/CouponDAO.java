fa74d8e183ad022fb025c6479f0a3e861682d8ea
周伟祥 <Ayanami@zhou>
2018-08-06 18:55:17 +0800

    在原有的CouponDAO、receiverAddressDAO上，在不修改原来同学的代码上，添加自己需要的功能

--- src/com/mi/model/dao/CouponDAO.java
+++ src/com/mi/model/dao/CouponDAO.java
@@ -3,6 +3,8 @@ package com.mi.model.dao;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.mi.model.bean.Coupon;
 
 public interface CouponDAO {
@@ -12,5 +14,6 @@ public interface CouponDAO {
 	public int selectValidateCoupon(Map<String, Object> map);
 	
 	public void updateReceiveCoupon(Map<String, Object> map);
-
+	
+	public List<Coupon> selectCouponByUserId(@Param("userId") int userId,@Param("couponStatus") int couponStatus);
 }
--- src/com/mi/model/dao/ReceiverAddressDAO.java
+++ src/com/mi/model/dao/ReceiverAddressDAO.java
@@ -2,10 +2,15 @@ package com.mi.model.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.mi.model.bean.ReceiverAddress;
 
 public interface ReceiverAddressDAO {
 	
 	public List<ReceiverAddress> selectAddress(Integer userId);
-
+	public List<ReceiverAddress> selectReceiverAddressByUserId(@Param("userId") int userId);
+	public void addReceiverAddress(ReceiverAddress receiverAddress);
+	public void deleteReceiverAddressById(@Param("receiverAddressId")int receiverAddressId);
+	public void updateReceiverAddress(ReceiverAddress receiverAddress);	
 }
